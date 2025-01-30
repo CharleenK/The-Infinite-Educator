5b490cefd6bf2164883dd043f54eb7d9fc180a33
CharleenK <kuchiburu@hotmail.com>
2025-01-30 17:27:03 -0500

    save before master merge

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Footer from "../Components/Footer";
 import "../styles/background.css";
+import styled from "styled-components";
 
 const Home = () => {
   return (
     <>
       <div className="gradient">
-        <h1> Welcome to The Infinite Educator</h1>
+        <StyledDiv1>
+          <h1> Welcome to The Infinite Educator</h1>
+        </StyledDiv1>
       </div>
       <Footer />
     </>
   );
 };
+
+const StyledDiv1 = styled.div`
+ 
+`;
+
 export default Home;
