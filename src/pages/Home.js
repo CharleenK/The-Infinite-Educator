04e9f51966f3b9963154e4cc6732d964ad0280b6
CharleenK <kuchiburu@hotmail.com>
2025-10-07 14:29:09 -0400

    font fizes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,44 +12,54 @@ const Home = () => {
     <>
       <div className="gradient">
         <StyledDiv1>
-          <h1> Welcome to The Infinite Educator</h1>
-          <h2>Childcare solutions that inspire!</h2>
+          <StyledH1> Welcome to The Infinite Educator</StyledH1>
+          <StyledH2>Childcare solutions that inspire!</StyledH2>
           <StyledDiv2>
-            <p>
-              Early childhood educators and early education teachers play a
-              crucial role in guiding our children during their formative
-              years,laying a strong foundation for a brighter future.
-            </p>
+            <StyledDiv3>
+              <p>
+                Early childhood educators and early education teachers play a
+                crucial role in guiding our children during their formative
+                years,laying a strong foundation for a brighter future.
+              </p>
+            </StyledDiv3>
             <StyledImg1 src={paintedChild} alt="child covered in paint" />
-            <p>
-              At the Infinite Educator, we offer workshops, seminars and
-              mentoring sessions designed to empower the early years community
-              to meet today's challenges and thrive in their classrooms.
-            </p>
+            <StyledDiv3>
+              <p>
+                At the Infinite Educator, we offer workshops, seminars and
+                mentoring sessions designed to empower the early years community
+                to meet today's challenges and thrive in their classrooms.
+              </p>
+            </StyledDiv3>
           </StyledDiv2>
           <h2>Ongoing development</h2>
           <StyledDiv2>
             <StyledImg2 src={storyTime} alt="teacher reads story to children" />
-            <p>
-              Professional development in early childhood education is a dynamic
-              journey of continuous learning and growth. The Infinite Educator
-              supports the early years community through innovative and creative
-              approaches to professional development.
-            </p>
-            <p>
-              Our workshops and mentoring sessions will inspire educators and
-              teachers with current research and hands on practical strategies
-              that can be easily implemented into the classroom.
-            </p>
+            <StyledDiv3>
+              <p>
+                Professional development in early childhood education is a
+                dynamic journey of continuous learning and growth. The Infinite
+                Educator supports the early years community through innovative
+                and creative approaches to professional development.
+              </p>
+            </StyledDiv3>
+            <StyledDiv3>
+              <p>
+                Our workshops and mentoring sessions will inspire educators and
+                teachers with current research and hands on practical strategies
+                that can be easily implemented into the classroom.
+              </p>
+            </StyledDiv3>
           </StyledDiv2>
           <h2>Tailored Programs</h2>
           <StyledDiv2>
             <StyledImg3 src={playBlocks} alt="blocks spell play" />
-            <p>
-              The Infinite Educator provides comprehensive workshops and
-              mentoring services that are specifically tailored to meet the
-              unique needs of your childcare facility or school.
-            </p>
+            <StyledDiv3>
+              <p>
+                The Infinite Educator provides comprehensive workshops and
+                mentoring services that are specifically tailored to meet the
+                unique needs of your childcare facility or school.
+              </p>
+            </StyledDiv3>
           </StyledDiv2>
         </StyledDiv1>
       </div>
@@ -58,6 +68,15 @@ const Home = () => {
   );
 };
 
+const StyledH1 = styled.h1`
+  font-size: 6vw;
+  color: rgba(16, 145, 87, 1);
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 3.5vw;
+  color: rgba(241, 254, 248, 1);
+`;
 const StyledDiv1 = styled.div`
   border: 2px solid blue;
   display: inline-flex;
@@ -74,6 +93,13 @@ const StyledDiv2 = styled.div`
   width: 95%;
   padding: 1.5%;
   text-align: center;
+`;
+
+const StyledDiv3 = styled.div`
+  border: 2px solid black;
+  width: 90%;
+  font-size: 2.5vw;
+  padding: 1.5%;
 `;
 
 const StyledImg1 = styled.img`
