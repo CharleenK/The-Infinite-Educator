11cd6aae6c51d50465e034871bbcba0229995187
CharleenK <kuchiburu@hotmail.com>
2025-10-09 12:23:41 -0400

    spacing

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,20 +17,21 @@ const About = () => {
       </StyledDivA>
       <StyledDiv1>
         <StyledDiv2>
-          {" "}
           <StyledH2>Our Guiding Principles</StyledH2>
           <StyledImg2
             src={teacherAndBalloons}
             alt="children and teachers playing with balloons"
           />
-          <p>
-            The Infinite Educator is dedicated to empowering educational
-            professionals through comprehensive support, proactive advocacy and
-            responsive listening strategies. Our core commitments include
-            providing targeted professional development resources, personalized
-            mentoring and promote inclusive and innovative educational
-            environments.
-          </p>
+          <StyledDiv3>
+            <p>
+              The Infinite Educator is dedicated to empowering educational
+              professionals through comprehensive support, proactive advocacy
+              and responsive listening strategies. Our core commitments include
+              providing targeted professional development resources,
+              personalized mentoring and promote inclusive and innovative
+              educational environments.
+            </p>
+          </StyledDiv3>
         </StyledDiv2>
         <StyledDiv2>
           <StyledH2>Our Mission</StyledH2>
@@ -38,11 +39,14 @@ const About = () => {
             src={teacherLaptop}
             alt="people gathered around a laptop, smiling"
           />
-          <p>
-            To improve educational quality in early childhood settings. Empower
-            educators with skills and knowledge and provide the early years
-            community comprehensive professional growth and new opportunities.
-          </p>
+          <StyledDiv3>
+            <p>
+              To improve educational quality in early childhood settings.
+              Empower educators with skills and knowledge and provide the early
+              years community comprehensive professional growth and new
+              opportunities.
+            </p>
+          </StyledDiv3>
         </StyledDiv2>
         <StyledDiv2>
           <StyledH3>Donna Skea</StyledH3>
@@ -51,25 +55,33 @@ const About = () => {
             Educator
           </h4>
           <StyledImg4 src={profilePic} alt="profile picture" />
-          <p>
-            With 30 years of experience in Early Childhood Education, I founded
-            The Infinite Educator to empower and support educators and childcare
-            centers.
-          </p>
-          <p>
-            Our mission is to build strong, positive communities within
-            childcare settings through tailored workshops and mentorship.
-          </p>
-          <p>
-            I offer guidance and strategies to foster a nurturing and
-            collaborative work environment, helping educators thrive and centers
-            succeed.
-          </p>
-          <p>
-            Ready to transform your childcare center? Book a FREE 30-minute
-            consultation today by reaching out via LinkedIn or email:
-            donna.skea30@gmail.com
-          </p>
+          <StyledDiv3>
+            <p>
+              With 30 years of experience in Early Childhood Education, I
+              founded The Infinite Educator to empower and support educators and
+              childcare centers.
+            </p>
+          </StyledDiv3>
+          <StyledDiv3>
+            <p>
+              Our mission is to build strong, positive communities within
+              childcare settings through tailored workshops and mentorship.
+            </p>
+          </StyledDiv3>
+          <StyledDiv3>
+            <p>
+              I offer guidance and strategies to foster a nurturing and
+              collaborative work environment, helping educators thrive and
+              centers succeed.
+            </p>
+          </StyledDiv3>
+          <StyledDiv3>
+            <p>
+              Ready to transform your childcare center? Book a FREE 30-minute
+              consultation today by reaching out via LinkedIn or email:
+              donna.skea30@gmail.com
+            </p>
+          </StyledDiv3>
         </StyledDiv2>
       </StyledDiv1>
     </>
@@ -92,11 +104,13 @@ const StyledH1 = styled.h1`
 const StyledH2 = styled.h2`
   font-size: 4vw;
   color: rgba(241, 254, 248, 1);
+  padding: 1%;
 `;
 
 const StyledH3 = styled.h3`
   font-size: 3vw;
   color: rgba(241, 254, 248, 1);
+  padding: 1%;
 `;
 
 const StyledDiv1 = styled.div`
@@ -117,7 +131,12 @@ const StyledDiv2 = styled.div`
   border: 2px solid green;
   border-radius: 25px;
   margin: 2.5%;
-  padding: 1.5%;
+  padding: 0% 1.5% 1.5%;
+`;
+
+const StyledDiv3 = styled.div`
+  border: 3px solid pink;
+  padding: 1%;
 `;
 
 const StyledImg1 = styled.img`
